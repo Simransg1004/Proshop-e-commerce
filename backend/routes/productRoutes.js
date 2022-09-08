5b86d24b3fb819765aad204a7fae94bd55ed31da
unknown <simransg100@gmail.com>
2022-09-08 14:48:51 +0530

    Custom error handling

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,6 +2,7 @@ import express from "express";
 import asyncHandler from "express-async-handler";
 const router = express.Router();
 import Product from "../models/productModel.js";
+// import errorHandler from "../middleware/errorMiddleware.js";
 
 //@desc     Fetch all products
 //@route    GET  /api/products
@@ -19,11 +20,11 @@ router.get(
 //@access   Public
 router.get("/:id", asyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id);
-
     if (product) {
       res.json(product);
     } else {
-      res.json({ message: "Product not found" });
+      res.status(404)
+      throw new Error("Product not found")
     }
   })
 );
--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
 import connectDB from "./config/db.js";
+import {notFound, errorHandler} from './middleware/errorMiddleware.js';
 import productRoutes from "./routes/productRoutes.js";
+import e from "express";
 
 dotenv.config();
 
@@ -15,6 +17,9 @@ app.get("/", (req, res) => {
 })
 
 app.use("/api/products", productRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
